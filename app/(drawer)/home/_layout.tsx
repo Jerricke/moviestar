4650eaae944518febbe91fea3c21d477b1531e09
Jerricke <eejerrickw@gmail.com>
2024-02-06 16:24:40 -0800

    theme-ing

--- app/(drawer)/home/_layout.tsx
+++ app/(drawer)/home/_layout.tsx
@@ -1,9 +1,30 @@
+import { DrawerToggleButton } from '@react-navigation/drawer';
+import { colorTokens } from '@tamagui/themes';
 import { Stack } from 'expo-router';
 import React from 'react';
 import { View, Text } from 'react-native';
+import { useTheme } from 'tamagui';
 
 const Layout = () => {
-  return <Stack />;
+  const theme = useTheme();
+
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: theme.blue7.get(),
+        },
+        headerTintColor: '#fff',
+      }}>
+      <Stack.Screen
+        name="index"
+        options={{
+          title: 'MovieStar',
+          headerLeft: () => <DrawerToggleButton tintColor="#fff" />,
+        }}
+      />
+    </Stack>
+  );
 };
 
 export default Layout;
--- app/(drawer)/home/index.tsx
+++ app/(drawer)/home/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { Card } from 'tamagui';
+
+import { Title } from '~/tamagui.config';
 
 const Page = () => {
   return (
     <View>
-      <Text>Home</Text>
+      <Title>Home</Title>
+      <Card>
+        <Card.Header>
+          <Text>Header</Text>
+        </Card.Header>
+        <Card.Footer />
+        <Card.Background />
+      </Card>
     </View>
   );
 };
