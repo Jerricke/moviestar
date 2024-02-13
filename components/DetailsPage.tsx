1c8768efd391a3917601b45c3acfcfb9f98545fb
Jerricke <eejerrickw@gmail.com>
2024-02-13 15:27:26 -0800

    details page and tv layout setup

--- components/DetailsPage.tsx
+++ components/DetailsPage.tsx
@@ -1,7 +1,10 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
-import { View, Text } from 'react-native';
+import { ImageBackground } from 'react-native';
+import { Main, ScrollView, Image, YStack, H1, Text, Paragraph } from 'tamagui';
 
 import { MediaType } from '~/interfaces/apiresults';
+import { getMovieDetails } from '~/services/api';
 
 type DetailsPageProps = {
   id: string;
@@ -9,10 +12,39 @@ type DetailsPageProps = {
 };
 
 const DetailsPage = ({ id, mediaType }: DetailsPageProps) => {
+  const movieQuery = useQuery({
+    queryKey: ['movie', id],
+    queryFn: () => getMovieDetails(+id, mediaType),
+  });
   return (
-    <View>
-      <Text>DetailsPage</Text>
-    </View>
+    <Main>
+      <ScrollView>
+        <ImageBackground
+          source={{ uri: `https://image.tmdb.org/t/p/w400${movieQuery.data?.backdrop_path}` }}>
+          <Image
+            m={20}
+            borderRadius={6}
+            source={{ uri: `https://image.tmdb.org/t/p/w400${movieQuery.data?.poster_path}` }}
+            w={200}
+            h={300}
+          />
+        </ImageBackground>
+      </ScrollView>
+      <YStack p={10} animation="lazy" enterStyle={{ opacity: 0, y: 0 }}>
+        <H1 color="$blue7">
+          {movieQuery.data?.title || movieQuery.data?.name}
+          <Text fontSize={16}>
+            (
+            {new Date(
+              movieQuery.data?.release_date! || movieQuery.data?.first_air_date!
+            ).getFullYear()}
+            )
+          </Text>
+        </H1>
+        <Paragraph theme="alt2">{movieQuery.data?.tagline}</Paragraph>
+        <Text fontSize={16}>{movieQuery.data?.overview}</Text>
+      </YStack>
+    </Main>
   );
 };
 
